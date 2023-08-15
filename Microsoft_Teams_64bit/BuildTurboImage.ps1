93c4eaa7e5995c9a43848d9c02abceec54206576
davetagg <124302945+davetagg@users.noreply.github.com>
2023-08-15 16:04:31 -0700

    Add files via upload

---
+++ Microsoft_Teams_64bit/BuildTurboImage.ps1
@@ -0,0 +1,111 @@
+## This script will download the latest installer and create a Turbo SVM image in @DESKTOP@\Package\TurboCapture.
+## The script is logged to @DESKTOP@\Package\Log.
+## The turbo project and build are saved  to @DESKTOP@\Package\TurboCapture.
+## Usage:
+## Run this script from an elevated cmd prompt: Powershell -ExecutionPolicy Bypass -File <path>\scriptname.ps1
+## Required:  You must have your Turbo Studio license in a "License.txt" file in an "Include" folder in the same folder as this script.
+## Required:  You must have the "GlobalBuildScript.ps1" file in an "Include" folder in the same folder as this script.
+## Required:  Any files used to customize the configuration should be a "Support Files" folder located in the same folder as this script.
+
+. "$PSScriptRoot\Include\GlobalBuildScript.ps1"  # Include the script that contains global variables and functions
+$scriptPath = $PSScriptRoot  # The folder path the script was launched from
+$SupportFiles = "$scriptPath\SupportFiles"  # The folder path contains files specific to this application build
+
+
+###################################
+## Define app specific variables ##
+###################################
+# These values will used to set the Metadata for the turbo image.
+
+$HubOrg = "microsoft/teams-x64"  # Set this for each package
+$Vendor = "Microsoft"
+$AppDesc = "Meet, chat, and share content with anyone from anywhere in an easy and reliable way."
+$AppName = "Teams (64-bit)"
+$VendorURL = "https://teams.microsoft.com/"
+
+
+##########################################
+## Download latest version of installer ##
+##########################################
+WriteLog "Downloading the latest MSI installer."
+
+# Get installer link for latest version
+$DownloadLink = "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true"
+
+# Folder the installer will be downloaded to
+$DownloadPath = New-Item -Path $scriptPath -Name "Installer" -ItemType "directory" -Force # create an Installer directory on the desktop for the donwnload
+# Name of the downloaded installer file
+$InstallerName = "Teams_windows_x64.msi"
+
+$Installer = DownloadInstaller $DownloadLink $DownloadPath $InstallerName
+
+#########################
+## Start Turbo Capture ##
+#########################
+
+StartTurboCapture
+
+#############################
+## Install the application ##
+#############################
+WriteLog "Installing the application."
+
+$ProcessExitCode = RunProcess "msiexec.exe" "/I $Installer ALLUSERS=1 /qn" $True
+CheckForError "Checking process exit code:" 0 $ProcessExitCode $True # Fail on install error
+
+&reg.exe add "HKCU\Software\Policies\Microsoft\Office\16.0\Teams" /t REG_DWORD /d 1 /v PreventFirstLaunchAfterInstall /f
+
+$ProcessExitCode = RunProcess "C:\Program Files (x86)\Teams Installer\Teams.exe" "--noui --noapplaunch" $True
+CheckForError "Checking process exit code:" 0 $ProcessExitCode $True # Fail on install error
+
+# Wait for the Update.exe process to start
+$processName = "Update"
+$processStarted = $false
+while (-not $processStarted) {
+    WriteLog "Waiting for $processName processes to start..."
+    $runningProcesses = Get-Process -Name $processName -ErrorAction SilentlyContinue
+    if ($runningProcesses) {
+        $processStarted = $true
+    }
+    Start-Sleep -Seconds 5
+}
+
+# Loop until there are no Update.exe processes
+while (Get-Process -Name $processName -ErrorAction SilentlyContinue) {
+    WriteLog "Waiting for $processName processes to finish..."
+    Start-Sleep -Seconds 5
+}
+
+
+################################
+## Customize the application  ##
+################################
+WriteLog "Performing post-install customizations."
+
+
+# Get the installed version from the registry
+foreach ($subkey in Get-ChildItem ("HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall")) {
+    $name = (Get-ItemProperty $subkey.PSPath).DisplayName
+    if ($name -eq "Teams Machine-Wide Installer") {
+        $InstalledVersion = (Get-ItemProperty $subkey.PSPath).DisplayVersion
+    }
+}
+
+#########################
+## Stop Turbo Capture  ##
+#########################
+
+StopTurboCapture
+
+######################
+## Customize XAPPL  ##
+######################
+
+CustomizeTurboXappl "$SupportFiles\PostCaptureModifications.ps1"  # Helper script for XML changes to Xappl"
+
+#########################
+## Build Turbo Image   ##
+#########################
+
+BuildTurboSvmImage
+
