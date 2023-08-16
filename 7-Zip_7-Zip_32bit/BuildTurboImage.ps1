958af4ded45a7b0fac5597f1f8f3770f6b3303a8
davetagg <124302945+davetagg@users.noreply.github.com>
2023-08-16 14:39:11 -0700

    Add files via upload

---
+++ 7-Zip_7-Zip_32bit/BuildTurboImage.ps1
@@ -0,0 +1,91 @@
+## This script will download the latest installer and create a Turbo SVM image in @DESKTOP@\Package\TurboCapture.
+## The script is logged to @DESKTOP@\Package\Log.
+## The turbo project and build are saved  to @DESKTOP@\Package\TurboCapture.
+## Usage:
+## Run this script from an elevated cmd prompt: Powershell -ExecutionPolicy Bypass -File <path>\scriptname.ps1
+## Required:  You must have your Turbo Studio license in a "License.txt" file in an "Include" folder in the same folder as this script.
+## Required:  You must have the "GlobalBuildScript.ps1" file in an "Include" folder in the same folder as this script.
+## Required:  Any files used to customize the configuration should be a "Support Files" folder located in the same folder as this script.
+
+. "$PSScriptRoot\Include\GlobalBuildScript.ps1"  # Include the script that contains global variables and functions
+$scriptPath = $PSScriptRoot  # The folder path the script was launched from
+$SupportFiles = "$scriptPath\SupportFiles"  # The folder path contains files specific to this application build
+
+
+###################################
+## Define app specific variables ##
+###################################
+# These values will used to set the Metadata for the turbo image.
+
+$HubOrg = "7-zip/7-zip"  # Set this for each package
+$Vendor = "7-Zip"
+$AppDesc = "Open source file archiver and compression tool."
+$AppName = "7-Zip (32-bit)"
+$VendorURL = "https://7-zip.org/"
+
+
+##########################################
+## Download latest version of installer ##
+##########################################
+WriteLog "Downloading the latest MSI installer."
+
+$Page = Invoke-WebRequest -Uri 'https://www.7-zip.org/download.html' -UseBasicParsing
+
+# Get installer link for latest version
+$LatestInstaller = ($Page.Links | Where-Object {$_.href -like "*.msi"})[1].href
+$DownloadLink = "https://www.7-zip.org/" + $LatestInstaller
+
+# Folder the installer will be downloaded to
+$DownloadPath = New-Item -Path $scriptPath -Name "Installer" -ItemType "directory" -Force # create an Installer directory on the desktop for the donwnload
+
+# Name of the downloaded installer file
+$InstallerName = $LatestInstaller.Split("/")[1]
+
+$Installer = DownloadInstaller $DownloadLink $DownloadPath $InstallerName
+
+#########################
+## Start Turbo Capture ##
+#########################
+
+StartTurboCapture
+
+#############################
+## Install the application ##
+#############################
+WriteLog "Installing the application."
+
+$ProcessExitCode = RunProcess "msiexec.exe" "/I $Installer ALLUSERS=1 /qn" $True
+CheckForError "Checking process exit code:" 0 $ProcessExitCode $True # Fail on install error
+
+################################
+## Customize the application  ##
+################################
+WriteLog "Performing post-install customizations."
+
+
+# Get the installed version from the registry
+foreach ($subkey in Get-ChildItem ("HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall")) {
+    $name = (Get-ItemProperty $subkey.PSPath).DisplayName
+    if ($name -match "7-Zip") {
+        $InstalledVersion = (Get-ItemProperty $subkey.PSPath).DisplayVersion
+    }
+}
+
+#########################
+## Stop Turbo Capture  ##
+#########################
+
+StopTurboCapture
+
+######################
+## Customize XAPPL  ##
+######################
+
+CustomizeTurboXappl "$SupportFiles\PostCaptureModifications.ps1"  # Helper script for XML changes to Xappl"
+
+#########################
+## Build Turbo Image   ##
+#########################
+
+BuildTurboSvmImage
+
