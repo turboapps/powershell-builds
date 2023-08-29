97afe73a6b68b9b3a004326ab3b9ac47311de86b
davetagg <124302945+davetagg@users.noreply.github.com>
2023-08-29 16:53:50 -0700

    Update BuildTurboImage.ps1

--- 7-Zip_7-Zip_32bit/BuildTurboImage.ps1
+++ 7-Zip_7-Zip_32bit/BuildTurboImage.ps1
@@ -63,7 +63,20 @@ CheckForError "Checking process exit code:" 0 $ProcessExitCode $True # Fail on i
 ################################
 WriteLog "Performing post-install customizations."
 
-
+# Associate file types with 7zFM.exe
+  &cmd.exe /C assoc .7z=7-Zip.7z
+  &cmd.exe /C --% ftype 7-Zip.7z="C:\Program Files (x86)\7-Zip\7zFM.exe" "%1"
+  &cmd.exe /C assoc .zip=7-Zip.zip
+  &cmd.exe /C --% ftype 7-Zip.zip="C:\Program Files (x86)\7-Zip\7zFM.exe" "%1"
+  &cmd.exe /C assoc .bz2=7-Zip.bz2
+  &cmd.exe /C --% ftype 7-Zip.bz2="C:\Program Files (x86)\7-Zip\7zFM.exe" "%1"
+  &cmd.exe /C assoc .gz=7-Zip.gz
+  &cmd.exe /C --% ftype 7-Zip.gz="C:\Program Files (x86)\7-Zip\7zFM.exe" "%1"
+  &cmd.exe /C assoc .tar=7-Zip.tar
+  &cmd.exe /C --% ftype 7-Zip.tar="C:\Program Files (x86)\7-Zip\7zFM.exe" "%1"
+  &cmd.exe /C assoc .tgz=7-Zip.tgz
+  &cmd.exe /C --% ftype 7-Zip.tgz="C:\Program Files (x86)\7-Zip\7zFM.exe" "%1"
+  
 # Get the installed version from the registry
 foreach ($subkey in Get-ChildItem ("HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall")) {
     $name = (Get-ItemProperty $subkey.PSPath).DisplayName
