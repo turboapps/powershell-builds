05381f5ca03bd5f0ef76d05c08cb31b34ca3d8bd
davetagg <124302945+davetagg@users.noreply.github.com>
2023-09-19 09:27:29 -0700

    Add files via upload

---
+++ Microsoft_SqlServerManagementStudio/BuildTurboImage.ps1
@@ -0,0 +1,89 @@
+## This script will download the latest installer and create a Turbo SVM image in @DESKTOP@\Package\TurboCapture.
+## The script is logged to @DESKTOP@\Package\Log.
+## The turbo project and build are saved  to @DESKTOP@\Package\TurboCapture.
+## Usage:
+## Run this script from an elevated cmd prompt: Powershell -ExecutionPolicy Bypass -File <path>\scriptname.ps1
+## Required:  You must have your Turbo Studio license in a "License.txt" file in an "Include" folder in the same folder as this script.
+## Required:  You must have the "GlobalBuildScript.ps1" file in an "Include" folder in the same folder as this script.
+## Required:  Any files used to customize the configuration should be a "Support Files" folder located in the same folder as this script.
+
+$scriptPath = $PSScriptRoot  # The folder path the script was launched from
+$GlobalScriptPath = Join-Path -Path $scriptPath -ChildPath "..\_INCLUDE\GlobalBuildScript.ps1"  #Get the path to the GlobalBuildScript.ps1
+. $GlobalScriptPath  # Include the script that contains global variables and functions
+$SupportFiles = "$scriptPath\SupportFiles"  # The folder path contains files specific to this application build
+
+
+###################################
+## Define app specific variables ##
+###################################
+# These values will used to set the Metadata for the turbo image.
+
+$HubOrg = "sqlserver/ssms"  # Set this for each package
+$Vendor = "Microsoft"
+$AppDesc = "SSMS is an integrated environment for configuring, managing, and developing for SQL Server."
+$AppName = "SQL Server Management Studio"
+$VendorURL = "https://learn.microsoft.com/en-us/sql/ssms/sql-server-management-studio-ssms?view=sql-server-ver16"
+
+
+##########################################
+## Download latest version of installer ##
+##########################################
+WriteLog "Downloading the latest installer."
+
+# Get installer link for latest version
+$DownloadLink = "https://aka.ms/ssmsfullsetup"
+# Folder the installer will be downloaded to
+$DownloadPath = New-Item -Path $scriptPath -Name "Installer" -ItemType "directory" -Force # create an Installer directory on the desktop for the donwnload
+# Name of the downloaded installer file
+$InstallerName = "SSMS-Setup-ENU.exe"
+
+$Installer = DownloadInstaller $DownloadLink $DownloadPath $InstallerName
+
+#########################
+## Start Turbo Capture ##
+#########################
+
+StartTurboCapture
+
+#############################
+## Install the application ##
+#############################
+WriteLog "Installing the application."
+
+$ProcessExitCode = RunProcess "$Installer" "DoNotInstallAzureDataStudio=1 /S" $True
+CheckForError "Checking process exit code:" 0 $ProcessExitCode $True # Fail on install error
+
+################################
+## Customize the application  ##
+################################
+WriteLog "Performing post-install customizations."
+
+
+
+# Get the installed version from the registry
+foreach ($subkey in Get-ChildItem ("HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall")) {
+    $name = (Get-ItemProperty $subkey.PSPath).DisplayName
+    if ($name -contains "SQL Server Management Studio") {
+        $InstalledVersion = (Get-ItemProperty $subkey.PSPath).DisplayVersion
+    }
+}
+
+
+#########################
+## Stop Turbo Capture  ##
+#########################
+
+StopTurboCapture
+
+######################
+## Customize XAPPL  ##
+######################
+
+CustomizeTurboXappl "$SupportFiles\PostCaptureModifications.ps1"  # Helper script for XML changes to Xappl"
+
+#########################
+## Build Turbo Image   ##
+#########################
+
+BuildTurboSvmImage
+
---
+++ Microsoft_SqlServerManagementStudio/SupportFiles/PostCaptureModifications.ps1
@@ -0,0 +1,104 @@
+
+################
+#   FUNCTIONS  #
+################
+
+# Adds a registry key to a parent - usage example: AddRegKey "Key[@name='@HKCU@']/Key[@name='Software']/Key[@name='Google']" "Chrome" "Full" "False" "False"
+Function AddRegKey($parentKey,$name,$isolation,$readOnly,$hide,$noSync) {
+$parentNode = $Registry.SelectSingleNode($parentKey)
+$node = $xappl.CreateElement("Key")
+$node.SetAttribute("name",$name)
+$node.SetAttribute("isolation",$isolation)
+$node.SetAttribute("readOnly",$readOnly)
+$node.SetAttribute("hide",$hide)
+$node.SetAttribute("noSync",$noSync)
+$parentNode.AppendChild($node)
+}
+
+# Adds a registry value to a parent - usage example: AddRegValue "Key[@name='@HKCU@']/Key[@name='Software']/Key[@name='Google']" "Chrome" "Full" "False" "False" "String" "some string"
+Function AddRegValue($parentKey,$name,$isolation,$readOnly,$hide,$type,$value) {
+$parentNode = $Registry.SelectSingleNode($parentKey)
+$node = $xappl.CreateElement("Value")
+$node.SetAttribute("name",$name)
+$node.SetAttribute("isolation",$isolation)
+$node.SetAttribute("readOnly",$readOnly)
+$node.SetAttribute("hide",$hide)
+$node.SetAttribute("type",$type)
+$node.SetAttribute("value",$value)
+$parentNode.AppendChild($node)
+}
+
+# Modifies an existing registry value - usage example: EditRegValue "Key[@name='@HKCU@']/Key[@name='Software']/Key[@name='Google']/Value[@name='Version']" "Full" "False" "False" "String" "102.1.4"
+Function EditRegValue($parentKey,$isolation,$readOnly,$hide,$type,$value) {
+$node = $Registry.SelectSingleNode($parentKey)
+$node.SetAttribute("isolation",$isolation)
+$node.SetAttribute("readOnly",$readOnly)
+$node.SetAttribute("hide",$hide)
+$node.SetAttribute("type",$type)
+$node.SetAttribute("value",$value)
+}
+
+#######################
+
+# Configure vm settings
+$VirtualizationSettings = $xappl.Configuration.SelectSingleNode("VirtualizationSettings")
+## $VirtualizationSettings.chromiumSupport = [string]$true  ## NOT REQUIRED 
+$virtualizationSettings.isolateWindowClasses = [string]$true
+$virtualizationSettings.launchChildProcsAsUser = [string]$true
+
+# Configure metadata for the application
+$StandardMetaData = $xappl.Configuration.SelectSingleNode("StandardMetadata")
+
+If ($AppName -ne $null){
+    $StandardMetaData.SelectNodes('StandardMetadataItem[@Name="Title"]') | ForEach-Object {$_.ParentNode.RemoveChild($_)}  # Remove entries if they exist
+    $node = $xappl.CreateElement("StandardMetadataItem") # Set Title meta
+    $node.SetAttribute("property","Title")
+    $node.SetAttribute("value",$AppName)
+    $StandardMetaData.AppendChild($node)
+}
+If ($Vendor -ne $null){ 
+    $StandardMetaData.SelectNodes('StandardMetadataItem[@Name="Publisher"]') | ForEach-Object {$_.ParentNode.RemoveChild($_)}
+    $node = $xappl.CreateElement("StandardMetadataItem") # Set Publisher meta
+    $node.SetAttribute("property","Publisher")
+    $node.SetAttribute("value",$Vendor)
+    $StandardMetaData.AppendChild($node)
+}
+If ($AppDesc -ne $null){ 
+    $StandardMetaData.SelectNodes('StandardMetadataItem[@Name="Description"]') | ForEach-Object {$_.ParentNode.RemoveChild($_)}
+    $node = $xappl.CreateElement("StandardMetadataItem") # Set Description meta
+    $node.SetAttribute("property","Description")
+    $node.SetAttribute("value",$AppDesc)
+    $StandardMetaData.AppendChild($node)
+}
+If ($VendorURL -ne $null){ 
+    $StandardMetaData.SelectNodes('StandardMetadataItem[@Name="Website"]') | ForEach-Object {$_.ParentNode.RemoveChild($_)} 
+    $node = $xappl.CreateElement("StandardMetadataItem") # Set Website meta
+    $node.SetAttribute("property","Website")
+    $node.SetAttribute("value",$VendorURL)
+    $StandardMetaData.AppendChild($node)
+}
+If ($InstalledVersion -ne $null){
+    $StandardMetaData.SelectNodes('StandardMetadataItem[@Name="Version"]') | ForEach-Object {$_.ParentNode.RemoveChild($_)}
+    $node = $xappl.CreateElement("StandardMetadataItem") # Set Version meta
+    $node.SetAttribute("property","Version")
+    $node.SetAttribute("value",$InstalledVersion)
+    $StandardMetaData.AppendChild($node)
+}
+
+
+
+###################
+# Edit Filesystem #
+###################
+
+$Filesystem = $xappl.Configuration.Layers.SelectSingleNode("Layer[@name='Default']").SelectSingleNode("Filesystem")
+
+
+#################
+# Edit Registry #
+#################
+## NOTE: Beware of case sensitivity when making registry changes.  eg. The registry value type "String" requires an upper-case 'S'
+##       When specifying a registry value, "OpenWithProgids" is different from "OpenWithProgIds"
+
+$Registry = $xappl.Configuration.Layers.SelectSingleNode("Layer[@name='Default']").SelectSingleNode("Registry")
+
