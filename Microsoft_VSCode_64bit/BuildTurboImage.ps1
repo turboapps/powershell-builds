df48e7699e24611942ff02559b8e88833baa6378
davetagg <124302945+davetagg@users.noreply.github.com>
2023-09-27 16:47:32 -0700

    Update BuildTurboImage.ps1

--- Microsoft_VSCode_64bit/BuildTurboImage.ps1
+++ Microsoft_VSCode_64bit/BuildTurboImage.ps1
@@ -62,13 +62,18 @@ WriteLog "Performing post-install customizations."
 Copy-Item -Path "$SupportFiles\Code" -Destination "$env:APPDATA\" -Recurse -Force
 
 # Get the installed version from the registry
-foreach ($subkey in Get-ChildItem ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")) {
-    $name = (Get-ItemProperty $subkey.PSPath).DisplayName
-    if ($name -eq "Microsoft Visual Studio Code") {
-        $InstalledVersion = (Get-ItemProperty $subkey.PSPath).DisplayVersion
+$key = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
+$subKey = $key.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall")
+$subKeyNames = $subKey.GetSubKeyNames()
+foreach($name in $subKeyNames) {
+    $sub = $subKey.OpenSubKey($name)
+    $displayName = $sub.GetValue("DisplayName")
+    if($displayName -like "*Microsoft Visual Studio Code*") {
+        # Output the key name and display name
+        $InstalledVersion = $sub.GetValue("DisplayVersion")
+        Write-Output "Key: $name, Display Version: $InstalledVersion"
     }
 }
-
 #########################
 ## Stop Turbo Capture  ##
 #########################
