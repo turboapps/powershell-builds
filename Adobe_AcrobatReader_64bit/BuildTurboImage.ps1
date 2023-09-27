e3a74d9203089ee932772db62722a97d2a72e1ae
davetagg <124302945+davetagg@users.noreply.github.com>
2023-09-27 16:40:57 -0700

    Update BuildTurboImage.ps1

--- Adobe_AcrobatReader_64bit/BuildTurboImage.ps1
+++ Adobe_AcrobatReader_64bit/BuildTurboImage.ps1
@@ -99,13 +99,18 @@ WriteLog "Performing post-install customizations."
 &cmd.exe /c rmdir /S /Q "C:\program files\common files\adobe\acrobat\setup"
 
 # Get the installed version from the registry
-foreach ($subkey in Get-ChildItem ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")) {
-    $name = (Get-ItemProperty $subkey.PSPath).DisplayName
-    if ($name -eq "Adobe Acrobat (64-bit)") {
-        $InstalledVersion = (Get-ItemProperty $subkey.PSPath).DisplayVersion
+$key = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
+$subKey = $key.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall")
+$subKeyNames = $subKey.GetSubKeyNames()
+foreach($name in $subKeyNames) {
+    $sub = $subKey.OpenSubKey($name)
+    $displayName = $sub.GetValue("DisplayName")
+    if($displayName -like "*Adobe Acrobat*") {
+        # Output the key name and display name
+        $InstalledVersion = $sub.GetValue("DisplayVersion")
+        Write-Output "Key: $name, Display Version: $InstalledVersion"
     }
 }
-
 #########################
 ## Stop Turbo Capture  ##
 #########################
