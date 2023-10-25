f9c4b4ca17858b9f03dd8392138e2915a890f977
davetagg <taggart@turbo.net>
2023-10-25 16:23:32 -0700

    Signed-off-by: davetagg <taggart@turbo.net>

--- sqlserver_ssms/VersionCheck.ps1
+++ sqlserver_ssms/VersionCheck.ps1
@@ -13,7 +13,16 @@ $HubVersion = GetCurrentHubVersion $HubOrg
 # Use this section to get the latest version 
 # either from the vendor website or the downloaded installer file
 
-$LatestWebVersion = "0.0"
+# Get installer link for latest version
+$DownloadLink = "https://aka.ms/ssmsfullsetup"
+
+# Name of the downloaded installer file
+$InstallerName = "SSMS-Setup-ENU.exe"
+
+$Installer = DownloadInstaller $DownloadLink $DownloadPath $InstallerName
+
+$LatestWebVersion = Get-VersionFromExe "$Installer"
+$LatestWebVersion = RemoveTrailingZeros "$LatestWebVersion"
 
 WriteLog "WebVersion=$LatestWebVersion"
 
