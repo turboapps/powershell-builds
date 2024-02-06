084853f0c74f03f6e36985369cda08e5f8ad9dbd
davetagg <124302945+davetagg@users.noreply.github.com>
2024-02-06 15:22:01 -0800

    Update PostCaptureModifications.ps1

--- postgresql_postgresql/SupportFiles/PostCaptureModifications.ps1
+++ postgresql_postgresql/SupportFiles/PostCaptureModifications.ps1
@@ -110,11 +110,11 @@ $StartupFiles = $xappl.Configuration.SelectSingleNode("StartupFiles")
 # Uncheck any default startup files
 $StartupFiles.SelectSingleNode("StartupFile[@default='True']").default = 'False'
 
-# Add a new startup file for "C:\postgres\bin\postgres.exe" -D C:\pg-data
+# Add a new startup file for "C:\pgsql\run-postgre-sql.bat"
 $node = $xappl.CreateElement("StartupFile")
-$node.SetAttribute("node","@SYSDRIVE@\pgsql\bin\postgres.exe")
+$node.SetAttribute("node","C:\pgsql\run-postgre-sql.bat")
 $node.SetAttribute("tag","postgres")
-$node.SetAttribute("commandLine","-D %PGDATA%")
+$node.SetAttribute("commandLine","")
 $node.SetAttribute("default","True")
 $node.SetAttribute("architecture","AnyCpu")
 $StartupFiles.AppendChild($node)
