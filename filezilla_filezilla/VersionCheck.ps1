ef8ef36d230ce734971630875818c8d5b13d312c
olegsomphane <oleg@somphane.com>
2024-02-08 16:32:45 -0800

    Add FileZilla and -x64 scripts.

---
+++ filezilla_filezilla/VersionCheck.ps1
@@ -0,0 +1,34 @@
+Function RunVersionCheck {
+
+#############################################
+## Get the current Hub version for the app ##
+#############################################
+
+$HubVersion = GetCurrentHubVersion $HubOrg
+
+#############################################
+## Get latest version from the vendor site ##
+#############################################
+
+# Use this section to get the latest version 
+# either from the vendor website or the downloaded installer file
+
+# Get main download page for application.
+$Page = curl -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0' https://filezilla-project.org/download.php?show_all=1 -UseBasicParsing
+
+# Get the latest tag for Compare-Versions
+$LatestWebVersion = (($Page.Links | Where-Object {$_.href -like "https://*FileZilla_*_win32-setup.exe*"}).href).split("_")[1]
+
+# Remove trailing zeroes because Hub trims them, so need to match for Compare-Versions.
+$LatestWebVersion = RemoveTrailingZeros "$LatestWebVersion"
+
+WriteLog "WebVersion=$LatestWebVersion"
+
+
+###########################################
+## Compare latest version to hub version ##
+###########################################
+
+Compare-Versions $HubVersion $LatestWebVersion #Script will exit if Hub version is the same or newer.
+
+}
