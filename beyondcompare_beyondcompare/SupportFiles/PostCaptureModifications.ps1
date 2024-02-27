fa391982b65ab455915e601d1c39c9054da6fcbd
davetagg <taggart@turbo.net>
2024-02-27 08:35:54 -0800

    Signed-off-by: davetagg <taggart@turbo.net>

--- beyondcompare_beyondcompare/SupportFiles/PostCaptureModifications.ps1
+++ beyondcompare_beyondcompare/SupportFiles/PostCaptureModifications.ps1
@@ -110,3 +110,14 @@ $Filesystem.SelectNodes("Directory[@name='@DESKTOP@']/Directory[@name='Package']
 
 
 $Registry = $xappl.Configuration.Layers.SelectSingleNode("Layer[@name='Default']").SelectSingleNode("Registry")
+
+######################
+# Edit Startup Files #
+######################
+## Change the container startup file to BCompare.exe
+$StartupFiles = $xappl.Configuration.SelectSingleNode("StartupFiles")
+$StartupFiles.SelectSingleNode("StartupFile[@node='@WINDIR@\Installer\{44E72A8E-80FF-4B71-B049-3D28A07B63BF}\BCompare.ico']").default = 'False'
+$parentNode = $StartupFiles.SelectNodes("StartupFile[@node='@PROGRAMFILES@\Beyond Compare 4\BCompare.exe']")
+ForEach ($childNodes in $parentNode) {
+    $childNodes.SetAttribute("default", "True")
+}
