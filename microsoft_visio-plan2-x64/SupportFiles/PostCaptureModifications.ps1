dc312c017980d515e43434d14ba12e4b580640ed
olegsomphane <oleg@somphane.com>
2024-06-12 16:40:18 -0700

    Add scripts for project and visio.

---
+++ microsoft_visio-plan2-x64/SupportFiles/PostCaptureModifications.ps1
@@ -0,0 +1,19 @@
+$PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
+. $PostCaptureFunctionsPath  # Include the script that contains post capture functions
+
+###################
+# Edit Filesystem #
+###################
+
+# No changes.
+
+######################
+# Edit Startup Files #
+######################
+## Change the container startup file to WINWORD
+$StartupFiles = $xappl.Configuration.SelectSingleNode("StartupFiles")
+$parentNode = $StartupFiles.SelectNodes("StartupFile[@node='@PROGRAMFILES@\Microsoft Office\Office16\VISIO.EXE']")
+ForEach ($childNodes in $parentNode) {
+    $childNodes.SetAttribute("default", "True")
+}
+
