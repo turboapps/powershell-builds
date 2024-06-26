fd83ca35f1db87bcb02f9bf484acfb96c747a437
davetagg <124302945+davetagg@users.noreply.github.com>
2024-06-26 16:30:27 -0700

    updates to project and visio
    - updated the version scripts to use the screen scrape code from the office build
    - removed the workaround to add a startup file for project
    -uncommented the code to set the auto start for the startup file in project

--- microsoft_project-onlinedesktopclient-x64/SupportFiles/PostCaptureModifications.ps1
+++ microsoft_project-onlinedesktopclient-x64/SupportFiles/PostCaptureModifications.ps1
@@ -13,14 +13,11 @@ $PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\
 
 
 ## Change the container startup file to WINPROJ.EXE for Project.
+ $StartupFiles = $xappl.Configuration.SelectSingleNode("StartupFiles")
+ $parentNode = $StartupFiles.SelectNodes("StartupFile[@node='@PROGRAMFILES@\Microsoft Office\Office16\WINPROJ.EXE']")
+ ForEach ($childNodes in $parentNode) {
+    $childNodes.SetAttribute("default", "True")
+ }
 
-## Needs workaround due to bug in Office transform. Uncomment and remove workaround on next Studio release.
-# $StartupFiles = $xappl.Configuration.SelectSingleNode("StartupFiles")
-# $parentNode = $StartupFiles.SelectNodes("StartupFile[@node='@PROGRAMFILES@\Microsoft Office\Office16\WINPROJ.EXE']")
-# ForEach ($childNodes in $parentNode) {
-#    $childNodes.SetAttribute("default", "True")
-# }
 
-# Workaround is to manually add the startup file:
-AddStartupFile "@PROGRAMFILES@\Microsoft Office\Office16\WINPROJ.EXE" "WINPROJ" "" $True "AnyCpu"
 
--- microsoft_visio-plan2-x64/VersionCheck.ps1
+++ microsoft_visio-plan2-x64/VersionCheck.ps1
@@ -10,16 +10,49 @@ $HubVersion = GetCurrentHubVersion $HubOrg
 ## Get latest version from the vendor site ##
 #############################################
 
-$Page = curl 'https://www.microsoft.com/en-us/download/details.aspx?id=49117' -UseBasicParsing
+# Scrape the official MS office release page to get the latest "Current Channel" build version
+# This code was parsed from https://github.com/DataForNerds/public/blob/main/scripts/ms/msapps/m365buildnumbers.ps1
+$rootPage = "https://docs.microsoft.com/en-us/officeupdates/update-history-microsoft365-apps-by-date"
 
-# Get installer link for latest version
-$DownloadLink = ($Page.Links | Where-Object {$_.href -like "*download.microsoft.com*"})[0].href
-$InstallerName = $DownloadLink.Split("/")[-1]
+$pageData = Invoke-WebRequest $rootPage -UseBasicParsing
 
-# Download the Office deployment tool
-$Installer = DownloadInstaller $DownloadLink $DownloadPath $InstallerName
+If($pageData.StatusCode -ne 200) {
+    Throw "Error $($pageData.StatusCode) Getting Page Data"
+}
 
-$LatestWebVersion = Get-VersionFromExe $Installer
+$tables = [regex]::New('(?msi)<table>(?:.*?)<tbody>(.*?)<\/tbody>').Matches($pageData.Content)
+$versionHistoryRows = [regex]::New('(?msi)<tr>(.*?)<\/tr>').Matches($tables[1].Groups[1].Value)
+$m365Releases = New-Object System.Collections.ArrayList
+
+$rxInnerLink = [Regex]::New('(?msi)<a(?:[^>])*>(.*?)<\/a>')
+$rxVersionBuild = [Regex]::New('(?msi)Version (.*?) \(Build {1,}(.*?)\)')
+
+$versionHistoryRows.ForEach{
+
+    $cellData = [regex]::New('(?msi)<td(?:[^>]*)>(.*?)<\/td>').Matches($_.Groups[1].Value)
+    $channelCurrentLinks = $rxInnerLink.Matches($cellData[2].Groups[1].Value)
+    $allLinks = New-Object System.Collections.ArrayList
+    $allLinks.AddRange(@($channelCurrentLinks.groups.where{$_.Name -eq 1} | Select-Object @{Name="Channel";Expression={"Current"}},@{Name="Value";Expression={$_.Value}}))
+
+    $allLinks.ForEach{
+        $versionBuild = $rxVersionBuild.Matches($_.Value)
+            
+        $thisChannel = $_.Channel
+
+        $versionBuild.ForEach{
+            $m365Releases.Add(
+                [PSCustomObject]@{
+                    FullBuild = "16.0.$($_.Groups[2].Value)"
+                }
+            ) | Out-Null
+        }
+    }
+
+}
+
+$m365Releases = $m365Releases | Select-Object FullBuild -Unique -First 1
+
+$LatestWebVersion = $m365Releases.FullBuild
 $LatestWebVersion = RemoveTrailingZeros "$LatestWebVersion"
 
 WriteLog "WebVersion=$LatestWebVersion"
