7f39632837c6020679ea16d26622ad55592e2c13
olegsomphane <oleg@somphane.com>
2024-07-03 16:43:22 -0700

    Fix vscode containers running side-by-side
    Apply pipe isolation for vscode.

--- microsoft_vscode/SupportFiles/PostCaptureModifications.ps1
+++ microsoft_vscode/SupportFiles/PostCaptureModifications.ps1
@@ -1,2 +1,15 @@
 $PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
 . $PostCaptureFunctionsPath  # Include the script that contains post capture functions
+
+
+###############################
+# Edit Named Object Isolation #
+###############################
+
+# Isolate the vscode basednamedojbect to allow running multiple instances side-by-side.
+# Pipe is versioned, ex 39ba74c1-1.90.2-main-sock, 39ba74c1-1.89.1-main-sock.
+$NamedObjectIsolation = $xappl.Configuration.SelectSingleNode("NamedObjectIsolation")
+
+$node = $xappl.CreateElement("Exception")
+$node.SetAttribute("regex","39ba74c1-.*-main-sock")
+$NamedObjectIsolation.AppendChild($node)
