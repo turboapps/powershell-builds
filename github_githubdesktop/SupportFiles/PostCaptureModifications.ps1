58838c0e2b9237f3721e02223c9f5860db63ddab
davetagg <124302945+davetagg@users.noreply.github.com>
2024-07-10 11:06:49 -0700

    added build script for github desktop

---
+++ github_githubdesktop/SupportFiles/PostCaptureModifications.ps1
@@ -0,0 +1,14 @@
+$PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
+. $PostCaptureFunctionsPath  # Include the script that contains post capture functions
+
+# Configure vm settings
+$VirtualizationSettings = $xappl.Configuration.SelectSingleNode("VirtualizationSettings")
+$VirtualizationSettings.chromiumSupport = [string]$true
+
+###################
+# Edit Filesystem #
+###################
+
+# Delete the Squirrel update executable, this will prevent auto update checks
+# It will also prevent users from manually running update checks
+$Xappl.SelectSingleNode("//File[@name='Update.exe']").ParentNode.RemoveChild($Xappl.SelectSingleNode("//File[@name='Update.exe']"))
