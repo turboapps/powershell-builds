ab14f59146177a5d02a4179723fb1b9cc968f493
ncaracelo1 <noah@turbo.net>
2024-09-09 11:34:28 -0400

    Add jre dependency for tika

--- apache_tika/SupportFiles/PostCaptureModifications.ps1
+++ apache_tika/SupportFiles/PostCaptureModifications.ps1
@@ -1,6 +1,11 @@
 $PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
 . $PostCaptureFunctionsPath  # Include the script that contains post capture functions
 
+# Add Java Runtime Environment as dependency
+# Don't need to actually specify version or hash, client can still resolve latest version even without a specific hash but need to at least
+# put something with 32 bytes in the hash field for the dependency otherwise config won't load
+AddDependency "oracle" "jre" "" "0000000000000000000000000000000000000000000000000000000000000000" $False
+
 ######################
 # Edit Startup Files #
 ######################
