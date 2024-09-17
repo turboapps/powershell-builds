97ee549b4eb727ea136d7941699da4e31f8d2803
ncaracelo1 <noah@turbo.net>
2024-09-17 17:08:05 -0400

    Do not specify hard dependencies for tika and headless-extractor

--- apache_tika/SupportFiles/PostCaptureModifications.ps1
+++ apache_tika/SupportFiles/PostCaptureModifications.ps1
@@ -1,22 +1,6 @@
 $PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
 . $PostCaptureFunctionsPath  # Include the script that contains post capture functions
 
-# Add Java Runtime Environment as dependency
-
-# At the moment pushes with client fail unless dependency hashes are valid. However, client can still resolve latest version even with an invalid hash
-# if the hash field in the config is at least 32 bytes (config load fails otherwise)
-#AddDependency "oracle" "jre" "" "0000000000000000000000000000000000000000000000000000000000000000" $False
-
-# Get latest JRE info
-$repo = "oracle/jre"
-$version = GetCurrentHubVersion $repo
-$hash = GetCurrentHubHash $repo
-
-if ($version -and $hash)
-{
-    AddDependency "oracle" "jre" "$version" "$hash" $False
-}
-
 ######################
 # Edit Startup Files #
 ######################
--- turbo_headless-extractor/SupportFiles/PostCaptureModifications.ps1
+++ turbo_headless-extractor/SupportFiles/PostCaptureModifications.ps1
@@ -1,27 +1,11 @@
 $PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
 . $PostCaptureFunctionsPath  # Include the script that contains post capture functions
 
-# Add Chrome as dependency
-
-# At the moment pushes with client fail unless dependency hashes are valid. However, client can still resolve latest version even with an invalid hash
-# if the hash field in the config is at least 32 bytes (config load fails otherwise)
-#AddDependency "google" "chrome" "" "0000000000000000000000000000000000000000000000000000000000000000" $False
-
-# Get latest Chrome info
-$repo = "google/chrome"
-$version = GetCurrentHubVersion $repo
-$hash = GetCurrentHubHash $repo
-
-if ($version -and $hash)
-{
-    AddDependency "google" "chrome" "$version" "$hash" $False
-}
-
 ######################
 # Edit Startup Files #
 ######################
 
 # Since this is not an application install, we have to set the startup file manually.
 
-# Add a new startup file, so running the image will automatically run the script
+# Add a new startup file, so running the image will automatically run the script (need to have chrome installed or layered in)
 AddStartupFile "powershell" "" "C:\extractor\Extract.ps1" $True "AnyCpu"
