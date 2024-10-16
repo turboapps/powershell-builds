07257372981b261d93e6189467d4e4d0f2eeeff6
davetagg <124302945+davetagg@users.noreply.github.com>
2024-10-16 09:25:18 -0700

    Delete inkscape_inkscape directory
    retiring as there is no longer a 32bit build of InkScape

--- inkscape_inkscape/SupportFiles/PostCaptureModifications.ps1
+++
@@ -1,2 +0,0 @@
-$PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
-. $PostCaptureFunctionsPath  # Include the script that contains post capture functions
