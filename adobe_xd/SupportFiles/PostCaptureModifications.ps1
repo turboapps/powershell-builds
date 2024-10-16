111b7959f0654b63ff19feb266203ad5c83c61a2
davetagg <124302945+davetagg@users.noreply.github.com>
2024-10-16 09:26:04 -0700

    Delete adobe_xd directory
    retiring as this application has been sunset by Adobe.

--- adobe_xd/SupportFiles/PostCaptureModifications.ps1
+++
@@ -1,9 +0,0 @@
-$PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
-. $PostCaptureFunctionsPath  # Include the script that contains post capture functions
-
-
-# Configure vm settings
-$VirtualizationSettings = $xappl.Configuration.SelectSingleNode("VirtualizationSettings")
-$VirtualizationSettings.chromiumSupport = [string]$true
-$VirtualizationSettings.shutdownProcessTree = [string]$true
-
