cfe16001f31574cbf785e7972ba9d60f5294897d
davetagg <124302945+davetagg@users.noreply.github.com>
2024-11-20 10:36:20 -0800

    added build for webex-fedramp

---
+++ webex_webex-fedramp/SupportFiles/PostCaptureModifications.ps1
@@ -0,0 +1,13 @@
+$PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
+. $PostCaptureFunctionsPath  # Include the script that contains post capture functions
+
+#################
+# Edit Registry #
+#################
+## NOTE: Beware of case sensitivity when making registry changes.  eg. The registry value type "String" requires an upper-case 'S'
+##       When specifying a registry value, "OpenWithProgids" is different from "OpenWithProgIds"
+
+
+
+# Remove the reg key HKLM\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate if captured to prevent discrepancies with the version of WebView2 between the capture device and client device.
+$Registry.SelectNodes("Key[@name='@HKLM@']/Key[@name='SOFTWARE']/Key[@name='WOW6432Node']/Key[@name='Microsoft']/Key[@name='EdgeUpdate']") | ForEach-Object { $_.ParentNode.RemoveChild($_) }
