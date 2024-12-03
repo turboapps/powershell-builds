9b942f69189f561d7faf7dffc0a3928c68e455e5
davetagg <124302945+davetagg@users.noreply.github.com>
2024-12-03 15:45:54 -0800

    Update PostCaptureModifications.ps1

--- adobe_indesign/SupportFiles/PostCaptureModifications.ps1
+++ adobe_indesign/SupportFiles/PostCaptureModifications.ps1
@@ -7,3 +7,12 @@ $VirtualizationSettings = $xappl.Configuration.SelectSingleNode("VirtualizationS
 $VirtualizationSettings.chromiumSupport = [string]$true
 $VirtualizationSettings.shutdownProcessTree = [string]$true
 
+# Add the folder @APPDATALOCAL@\Temp to fix a permission error when using merge isolation
+# Can be removed when bug VM-2490 is resolved
+AddDirectory "Directory[@name='@APPDATALOCAL@']" "Temp" "WriteCopy" "False" "False"
+
+# Change folder and subfolders from Merge to WriteCopy isolation
+PushFolderIsolation "Directory[@name='@APPDATACOMMON@']/Directory[@name='Adobe']/descendant-or-self::*" "Merge" "WriteCopy"
+PushFolderIsolation "Directory[@name='@PROGRAMFILES@']/Directory[@name='Adobe']/descendant-or-self::*" "Merge" "WriteCopy"
+PushFolderIsolation "Directory[@name='@PROGRAMFILESCOMMON@']/Directory[@name='Adobe']/descendant-or-self::*" "Merge" "WriteCopy"
+PushFolderIsolation "Directory[@name='@PROGRAMFILESCOMMONX86@']/Directory[@name='Adobe']/descendant-or-self::*" "Merge" "WriteCopy"
