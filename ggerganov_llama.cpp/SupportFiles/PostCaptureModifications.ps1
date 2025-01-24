e0850ef022ed0585bafd9fbd62d766943b356220
davetagg <124302945+davetagg@users.noreply.github.com>
2025-01-24 14:12:00 -0800

    Update PostCaptureModifications.ps1

--- ggerganov_llama.cpp/SupportFiles/PostCaptureModifications.ps1
+++ ggerganov_llama.cpp/SupportFiles/PostCaptureModifications.ps1
@@ -8,7 +8,7 @@ $PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\
 # Since this is not an application install, we have to set the startup file manually.
 
 # Add startup files for all llama tools with triggers. None are startup files by default
-$Builds = "noavx","avx","avx2","avx512","cuda-cu12.2.0"
+$Builds = "noavx","avx","avx2","avx512","cuda-cu12.4"
 foreach ($Build in $Builds) {
     AddStartupFile "@SYSDRIVE@\llama-$Build\llama-cli.exe" "llama-cli-$Build" "" $False "AnyCpu"
     AddStartupFile "@SYSDRIVE@\llama-$Build\llama-server.exe" "llama-server-$Build" "" $False "AnyCpu"
