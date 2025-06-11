60434f4431d1e1f327f35d8dfd68aaa8cbf910e2
davetagg <124302945+davetagg@users.noreply.github.com>
2025-06-11 16:19:58 -0700

    Update PostCaptureModifications.ps1

--- microsoft_edgewebview2/SupportFiles/PostCaptureModifications.ps1
+++ microsoft_edgewebview2/SupportFiles/PostCaptureModifications.ps1
@@ -1,2 +1,21 @@
 $PostCaptureFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\!include\PostCaptureFunctions.ps1"
 . $PostCaptureFunctionsPath  # Include the script that contains post capture functions
+
+###################
+# Edit Filesystem #
+###################
+
+#Remove folder @PROGRAMFILESX86@\Microsoft\EdgeUpdate
+$Filesystem.SelectNodes("Directory[@name='@PROGRAMFILESX86@']/Directory[@name='Microsoft']/Directory[@name='EdgeUpdate']") | ForEach-Object { $_.ParentNode.RemoveChild($_) }
+
+#################
+# Edit Registry #
+#################
+## NOTE: Beware of case sensitivity when making registry changes.  eg. The registry value type "String" requires an upper-case 'S'
+##       When specifying a registry value, "OpenWithProgids" is different from "OpenWithProgIds"
+
+# Set Full isolation on HKLM\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate and subkeys
+$parentNode = $Registry.SelectNodes("Key[@name='@HKLM@']/Key[@name='SOFTWARE']/Key[@name='WOW6432Node']/Key[@name='Microsoft']/Key[@name='EdgeUpdate']/descendant-or-self::*")
+ForEach ($childNodes in $parentNode) {
+    $childNodes.SetAttribute("isolation", "Full")
+}
