2c519cbaef69916f6151e8d30f6b4e5236dd6a63
David Olsson <david@turbo.net>
2026-04-08 15:41:24 -0700

    use google api for chrome

--- google_chrome-x64/VersionCheck.ps1
+++ google_chrome-x64/VersionCheck.ps1
@@ -10,22 +10,8 @@ $HubVersion = GetCurrentHubVersion $HubOrg
 ## Get latest version from the vendor site ##
 #############################################
 
-# Have to download the installer and check its meta for the actual version.
-$DownloadLink = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B2CC0992A-8A31-8D75-167C-5C46238DE706%7D%26lang%3Den%26browser%3D5%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dtrue%26ap%3Dx64-stable-statsdef_0%26brand%3DGCEW/dl/chrome/install/googlechromestandaloneenterprise64.msi"
-
-# Name of the downloaded installer file
-$InstallerName = "googlechromestandaloneenterprise.msi"
-
-# Installer file object
-$Installer = DownloadInstaller $DownloadLink $DownloadPath $InstallerName
-
-# Read the comments field from the file meta using the Windows Shell object.
-# The MSI installer lists a different version under the MSI ProductVersion field that cannot be converted back to the Chrome version.
-$InstallerFolder = (New-Object -ComObject Shell.Application).NameSpace((Split-Path $Installer))
-## Set property index to 24 to get the Comments file meta field.
-## Split the comments at the Copyright part and grab the first string for the version.
-$LatestWebVersion = ($InstallerFolder.GetDetailsOf($InstallerFolder.parsename((Split-Path -Leaf $Installer)),24) -split " Copyright")[0]
-$LatestWebVersion = RemoveTrailingZeros "$LatestWebVersion"
+$release = Invoke-RestMethod "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows&num=1"
+$LatestWebVersion = RemoveTrailingZeros $release[0].version
 
 WriteLog "WebVersion=$LatestWebVersion"
 
--- google_chrome/VersionCheck.ps1
+++ google_chrome/VersionCheck.ps1
@@ -10,22 +10,8 @@ $HubVersion = GetCurrentHubVersion $HubOrg
 ## Get latest version from the vendor site ##
 #############################################
 
-# Have to download the installer and check its meta for the actual version.
-$DownloadLink = "https://dl.google.com/tag/s/appguid={00000000-0000-0000-0000-000000000000}&iid={00000000-0000-0000-0000-000000000000}&lang=$language&browser=3&usagestats=0&appname=Google%20Chrome&installdataindex=defaultbrowser&needsadmin=prefers/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
-
-# Name of the downloaded installer file
-$InstallerName = "googlechromestandaloneenterprise.msi"
-
-# Installer file object
-$Installer = DownloadInstaller $DownloadLink $DownloadPath $InstallerName
-
-# Read the comments field from the file meta using the Windows Shell object.
-# The MSI installer lists a different version under the MSI ProductVersion field that cannot be converted back to the Chrome version.
-$InstallerFolder = (New-Object -ComObject Shell.Application).NameSpace((Split-Path $Installer))
-## Set property index to 24 to get the Comments file meta field.
-## Split the comments at the Copyright part and grab the first string for the version.
-$LatestWebVersion = ($InstallerFolder.GetDetailsOf($InstallerFolder.parsename((Split-Path -Leaf $Installer)),24) -split " Copyright")[0]
-$LatestWebVersion = RemoveTrailingZeros "$LatestWebVersion"
+$release = Invoke-RestMethod "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows&num=1"
+$LatestWebVersion = RemoveTrailingZeros $release[0].version
 
 WriteLog "WebVersion=$LatestWebVersion"
 
