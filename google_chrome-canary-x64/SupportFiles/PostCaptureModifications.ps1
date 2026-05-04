f5a894a9451b4eef461435f6f2cdb9c6ce1203a5
davetagg <124302945+davetagg@users.noreply.github.com>
2026-05-04 10:39:40 -0700

    update chrome-canary build

--- google_chrome-canary-x64/SupportFiles/PostCaptureModifications.ps1
+++ google_chrome-canary-x64/SupportFiles/PostCaptureModifications.ps1
@@ -78,8 +78,15 @@ AddRegKey "Key[@name='@HKCU@']/Key[@name='Software']/Key[@name='Classes']" "http
 AddRegValue "Key[@name='@HKCU@']/Key[@name='Software']/Key[@name='Classes']/Key[@name='http']" "URL Protocol" "Full" "False" "False" "String" ""
 AddRegKey "Key[@name='@HKCU@']/Key[@name='Software']/Key[@name='Classes']" "https" "Full" "False" "False"
 AddRegValue "Key[@name='@HKCU@']/Key[@name='Software']/Key[@name='Classes']/Key[@name='https']" "URL Protocol" "Full" "False" "False" "String" ""
-# Clone the ChromeSSHTM.4HPY6QU5NX6S6GHX42D2W4VUCE ProgID node to http and https
-$chromeHtmlNode = $xappl.SelectSingleNode("//ProgId[@name='ChromeSSHTM.SHBV4SGIFAUWKZIVXJKOW5JBFM']").CloneNode($true)
+
+# Clone the ChromeSSHTM ProgID node to http and https
+# Find the ChromeSSHTM ProgId node dynamically
+$chromeHtmlNode = $xappl.SelectSingleNode("//ProgId[starts-with(@name,'ChromeSSHTM.')]")
+# Then clone it
+if ($chromeHtmlNode) {
+    $clonedNode = $chromeHtmlNode.CloneNode($true)
+}
+
 # Modify the ProgId name and description for HTTP and HTTPS
 $httpNode = $chromeHtmlNode.Clone()
 $httpNode.SetAttribute("name", "http")
