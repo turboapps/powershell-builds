bc8a442ae81e87beff6f36025bdfda2e08925cb1
davetagg <124302945+davetagg@users.noreply.github.com>
2026-09-23 14:30:06 -0700

    update temurin builds

--- eclipse_temurinjre/VersionCheck.ps1
+++ eclipse_temurinjre/VersionCheck.ps1
@@ -10,22 +10,25 @@ $HubVersion = GetCurrentHubVersion $HubOrg
 ## Get latest version from the vendor site ##
 #############################################
 
-# Get major version of the latest release
-$url = "https://api.adoptium.net/v3/info/available_releases"
-$response = curl -Uri $url -UseBasicParsing
-$jsonData = $response.Content | ConvertFrom-Json
-$latestMajorVersion = $jsonData.available_releases[-1]
-
-# Use the vendor API to get the latest release given the major version
 $Platform = 'windows'
 $Type = 'jre'
-$ReleaseInfo = Invoke-WebRequest -Uri "https://api.adoptium.net/v3/assets/latest/$latestMajorVersion/hotspot?architecture=x64&image_type=$Type&os=$Platform&vendor=eclipse" -UseBasicParsing | ConvertFrom-Json
 
-# Get the version from the Release json
-$majorVer = $ReleaseInfo.version.major
-$minorVer = $ReleaseInfo.version.minor
-$buildVer = $ReleaseInfo.version.build
-$LatestWebVersion = "$majorVer.$minorVer.$buildVer"
+# List of feature releases, newest first
+$releases = (Invoke-RestMethod -Uri "https://api.adoptium.net/v3/info/available_releases").available_releases | Sort-Object -Descending
+
+# The newest listed release may not have a GA build for this platform yet, so walk down until one does
+$ReleaseInfo = $null
+foreach ($major in $releases) {
+    $ReleaseInfo = Invoke-RestMethod -Uri "https://api.adoptium.net/v3/assets/latest/$major/hotspot?architecture=x64&image_type=$Type&os=$Platform&vendor=eclipse" |
+        Select-Object -First 1
+    if ($ReleaseInfo) { break }
+}
+if (-not $ReleaseInfo) { throw "No $Type release found for $Platform x64" }
+
+# Build the version string: major.minor.security (plus .patch when present, e.g. 26.0.2.1)
+$v = $ReleaseInfo.version
+$LatestWebVersion = "$($v.major).$($v.minor).$($v.security)"
+if ($v.patch) { $LatestWebVersion += ".$($v.patch)" }
 
 WriteLog "WebVersion=$LatestWebVersion"
 
